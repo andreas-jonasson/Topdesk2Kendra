f5feef9c1056497a55ef94e2d1a219947b74fe61
Andreas <andreas.jonasson@gmail.com>
2022-04-11 19:54:48 +0200

    First test..

---
+++ src/test/java/se/chalmers/topdesk/ConfigurationTest.java
@@ -0,0 +1,27 @@
+package se.chalmers.topdesk;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+class ConfigurationTest
+{
+    private Configuration configuration;
+
+    @BeforeEach
+    void setUp()
+    {
+         configuration = Configuration.getInstance();
+    }
+
+    @Test
+    @DisplayName("Configuration.getInstance() returns a Configuration")
+    public void getReminder_getExist()
+    {
+        assertNotNull(configuration, "Configuration.getInstance() did not return a valid instance.");
+        assertInstanceOf(Configuration.class, configuration, "Configuration.getInstance() did not return an instance of Configuration.");
+    }
+}
