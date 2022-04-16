637feb84b454e0b0de84c7aa0f518fa93353115c
Andreas <andreas.jonasson@gmail.com>
2022-04-16 20:56:06 +0200

    Prioritized a translation between TopDesk and Kendra information models.

--- src/test/java/se/chalmers/topdesk/model/KnowledgeItemTest.java
+++ src/test/java/se/chalmers/topdesk/model/KnowledgeItemTest.java
@@ -40,4 +40,19 @@ class KnowledgeItemTest
         assertNotNull(knowledgeItem.getContent());
         assertInstanceOf(String.class, knowledgeItem.getContent());
     }
+
+    /*
+        Mappings from TopDesk to Kendra metadata:
+        translation.creator.name 	->  _authors (String list) *
+        hmmm... Depends on domain?      _category (String) **
+        translation.creationDate 	->  _created_at (ISO 8601 encoded string) ***
+        "TopDesk" 			        ->  _data_source_id (String) ***
+        translation.content.content	->  _document_body (String) – The content of the document. ****
+        id || number 			    ->  _document_id (String) – A unique identifier for the document. ****
+        translation.content.title	->  _document_title (String) – The title of the document. ***
+        hmm... Maybe useful, or not.    _faq_id (String) – If this is an FAQ question and answer, a unique identifier for them. *
+        "html" 				        ->  _file_type (String) – The file type of the document, such as pdf or doc. ****
+        translation.modificationDate->  _last_updated_at (ISO 8601 encoded string) – The date and time that the document was last updated. ***
+        translation.language		->  _language_code (String) ****
+     */
 }
